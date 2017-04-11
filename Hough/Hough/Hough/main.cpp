47a7e039c1143948849f6187893eb0c99328e764
fatetail <leewp0815@gmail.com>
2017-04-11 16:09:32 +0800

    Hough
    Hough Transform

---
+++ Hough/Hough/Hough/main.cpp
@@ -0,0 +1,14 @@
+#include"Hough.h"
+#include<string>
+using namespace std;
+using namespace cimg_library;
+
+int main() {
+	string infile = "input3.bmp";
+	CImg<float> src(infile.c_str());
+	Hough h(src, 10.5f, 30, 1000, 60);
+	h.HoughProcess();
+	return 0;
+
+
+}
